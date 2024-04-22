66a38d0b1ff492292e756aed16f64875debdcf06
daghlani <32533115+daghlani@users.noreply.github.com>
2024-04-22 17:52:36 +0330

    update login_checker_installer script

--- login_checker_installer.sh
+++ login_checker_installer.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # download script
-wget 
+wget https://raw.githubusercontent.com/daghlani/login_checker/main/login_checker.sh -o login_checker.sh
 
 
 # chmod
-chmod +x 
+chmod +x login_checker.sh
+
+
 # download service
-
-
-
-# create log folder
+wget https://raw.githubusercontent.com/daghlani/login_checker/main/login_checker.service -o /etc/systemd/system/login_checker.service
 
 # create log file
+touch /var/log/login_checker.log
 
 # service working
+systemctl enable login_checker
+systemctl start login_checker
